cd884925ce3a7ba4d3ca11b086d03be99a1cf937
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-09-07 11:37:28 -0300

    Pequena correção em semestre

--- database/migrations/1662424802241_semestres.ts
+++ database/migrations/1662424802241_semestres.ts
@@ -6,9 +6,9 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('nome',30)
-      table.dateTime('data_inicio')
-      table.dateTime('data_fim')
+      table.string('nome',30).notNullable()
+      table.dateTime('data_inicio').notNullable()
+      table.dateTime('data_fim').notNullable()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
