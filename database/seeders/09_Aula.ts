ca2b4d7bfc0a936752e80544284b886854a0aa9a
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-09-19 20:07:15 -0300

    feito os seeders em ordem

--- database/seeders/09_Aula.ts
+++ database/seeders/09_Aula.ts
@@ -1,6 +1,5 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Aula from 'App/Models/Aula'
-import { DateTime } from 'luxon'
 
 export default class extends BaseSeeder {
   public async run () {
