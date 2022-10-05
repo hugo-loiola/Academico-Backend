2ca4481eeb631f145b6a0dd2aa417adbdc7b60f4
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-10-05 20:16:36 -0300

    :zap: criando os controllers de disciplina

--- app/Controllers/Http/DisciplinasController.ts
+++ app/Controllers/Http/DisciplinasController.ts
@@ -1,14 +1,33 @@
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-import Disciplina from 'App/Models/Disciplina'
+import Disciplina from "App/Models/Disciplina";
 
 export default class DisciplinasController {
-  index (){
-    return Disciplina.all()
+  async index() {
+    return await Disciplina.all();
   }
-  store ({request}){
-    const dados = request.only(['nome', 'cursoId'])
-    return Disciplina.create(dados)
+  async store({ request }) {
+    const dados = request.only(["nome", "cursoId"]);
+    return await Disciplina.create(dados);
+  }
+  async show({ request }) {
+    const id = request.param("id");
+    return await Disciplina.findOrFail(id);
+  }
+  async destroy({ request }) {
+    const id = request.param("id");
+    const disciplina = await Disciplina.findOrFail(id);
+
+    disciplina.delete();
+  }
+  async update({ request }) {
+    const id = request.param("id");
+    const disciplina = await Disciplina.findOrFail(id);
+    const dados = request.only(["nome", "cursoId"]);
+
+    disciplina.merge(dados);
+
+    return await disciplina.save();
   }
 }
