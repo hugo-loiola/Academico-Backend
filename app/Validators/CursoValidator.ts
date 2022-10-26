158c9a764e9520d4714126ccc544004beefbaeed
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-10-26 20:30:05 -0300

    validator curso

--- app/Validators/CursoValidator.ts
+++ app/Validators/CursoValidator.ts
@@ -1,13 +1,13 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class CursoValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    nome: schema.string(),
-    duracao: schema.number(),
-    modalidade: schema.string(),
+    nome: schema.string({}, [rules.alpha()]),
+    duracao: schema.number.optional(),
+    modalidade: schema.string({}, [rules.alpha()]),
   })
 
   public messages: CustomMessages = {}
