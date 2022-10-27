3cdef70a5c2d1f76940e25c3654bc959af8ee521
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-10-27 17:35:09 -0300

    validator professor

--- app/Validators/ProfessorValidator.ts
+++ app/Validators/ProfessorValidator.ts
@@ -1,4 +1,4 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ProfessorValidator {
@@ -23,7 +23,32 @@ export default class ProfessorValidator {
    *     ])
    *    ```
    */
-  public schema = schema.create({})
+  public schema = schema.create({
+    nome: schema.string([rules.alpha({ allow: ['space'] }), rules.maxLength(100)]),
+
+    cpf: schema.string([rules.alphaNum({ allow: ['space', 'dash', 'underscore'] })]),
+
+    matricula: schema.string([
+      rules.alphaNum({ allow: ['space', 'dash', 'underscore'] }),
+      rules.maxLength(20),
+    ]),
+
+    salario: schema.number.optional([rules.range(1, 10000)]),
+
+    email: schema.string.optional([rules.email(), rules.maxLength(100)]),
+
+    telefone: schema.string.optional([rules.mobile({ locale: ['pt-BR'] })]),
+
+    cep: schema.number.optional(),
+
+    logradouro: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+
+    complemento: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+
+    numero: schema.string.optional([rules.maxLength(20), rules.alphaNum({ allow: ['dash'] })]),
+
+    bairro: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+  })
 
   /*
    * Custom messages for validation failures. You can make use of dot notation `(.)`
