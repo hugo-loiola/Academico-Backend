f20368fc3f217f23d33957dc9da88ee4a8cce408
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-07 20:28:34 -0300

    user controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,10 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class UsersController {}
+import User from 'App/Models/User'
+
+export default class UsersController {
+  async store({ request }) {
+    const dados = request.only(['email', 'password'])
+    return await User.create(dados)
+  }
+}
