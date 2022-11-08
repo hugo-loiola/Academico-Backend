7dcc54143af9ea83c6d0898f7898697c302abd52
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-08 13:47:04 -0300

    kernel

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,6 +3,9 @@
 import User from 'App/Models/User'
 
 export default class UsersController {
+  async index() {
+    return await User.query()
+  }
   async store({ request }) {
     const dados = request.only(['email', 'password'])
     return await User.create(dados)
@@ -10,6 +13,6 @@ export default class UsersController {
   async login({ request, auth }) {
     const { email, password } = request.body()
 
-    return await auth.user('api').attempt(email, password)
+    return await auth.use('api').attempt(email, password)
   }
 }
--- start/kernel.ts
+++ start/kernel.ts
@@ -20,9 +20,7 @@ import Server from '@ioc:Adonis/Core/Server'
 | are defined for every HTTP requests.
 |
 */
-Server.middleware.register([
-  () => import('@ioc:Adonis/Core/BodyParser'),
-])
+Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
 
 /*
 |--------------------------------------------------------------------------
@@ -40,5 +38,5 @@ Server.middleware.register([
 | Route.get('dashboard', 'UserController.dashboard').middleware('auth')
 |
 */
-Server.middleware.registerNamed({
-})
+
+Server.middleware.registerNamed({ auth: () => import('App/Middleware/Auth') })
