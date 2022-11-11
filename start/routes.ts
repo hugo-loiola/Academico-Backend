12819b8e7e02103a9e6824561a5c7a4a6339a8bb
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-11 20:55:29 -0300

    :tada: feito

--- start/routes.ts
+++ start/routes.ts
@@ -20,11 +20,7 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
-
-Route.post('/users', 'UsersController.store').middleware('auth')
+Route.post('/users', 'UsersController.store')
 Route.post('/login', 'UsersController.login')
 
 Route.group(() => {
