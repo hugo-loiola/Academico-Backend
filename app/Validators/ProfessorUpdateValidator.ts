1ed156dd9c7bcf78ede30614369a1988c83e6db0
Lugia8 <hugoqueiroz2412@gmail.com>
2022-11-18 12:47:51 -0300

    make in professorUpdate

--- app/Validators/ProfessorUpdateValidator.ts
+++ app/Validators/ProfessorUpdateValidator.ts
@@ -1,40 +1,46 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ProfessorUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
-  public schema = schema.create({})
+  public schema = schema.create({
+    nome: schema.string([rules.alpha({ allow: ['space'] }), rules.maxLength(100)]),
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
-  public messages: CustomMessages = {}
+    cpf: schema.string([rules.regex(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)]),
+
+    matricula: schema.string([
+      rules.alphaNum({ allow: ['space', 'dash', 'underscore'] }),
+      rules.maxLength(20),
+    ]),
+
+    salario: schema.number.optional([rules.range(1, 10000)]),
+
+    email: schema.string.optional([rules.email(), rules.maxLength(100)]),
+
+    telefone: schema.string.optional([
+      rules.mobile({ locale: ['pt-BR'] }),
+      rules.regex(/^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/),
+    ]),
+
+    cep: schema.number.optional([rules.regex(/[0-9]{5}-[\d]{3}/)]),
+
+    logradouro: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+
+    complemento: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+
+    numero: schema.number.optional([rules.range(1, 10000)]),
+
+    bairro: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+  })
+
+  public messages: CustomMessages = {
+    'maxLength': 'o número máximo de caractéres do campo {{ field }} é de {{ options.maxLength }}',
+    'minLength': 'o número máximo de caractéres do campo {{ field }} é de {{ options.minLength }}',
+    'required': 'o campo {{ field }} é obrigatório',
+    'unique': 'o campo {{ field }} é único',
+    'cpf.regex': 'o cpf tem a formatação 666.666.666-13',
+    'cep.regex': 'o cep tem a formatação 12345-67',
+    'telefone.regex': 'o telefone tem a formatação (00)9999-9999',
+  }
 }
