a6b13581a108213a9056f96afc070cbcced3c958
Lugia8 <hugoqueiroz2412@gmail.com>
2022-11-18 12:45:49 -0300

    make in chamadaUpdate

--- app/Validators/ChamadaUpdateValidator.ts
+++ app/Validators/ChamadaUpdateValidator.ts
@@ -1,40 +1,23 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ChamadaUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
-  public schema = schema.create({})
+  public schema = schema.create({
+    aula_id: schema.number([
+      rules.unique({ table: 'aulas', column: 'id' }),
+      rules.exists({ table: 'aulas', column: 'id' }),
+    ]),
+    aluno_id: schema.number([
+      rules.unique({ table: 'alunos', column: 'id' }),
+      rules.exists({ table: 'alunos', column: 'id' }),
+    ]),
+    presenca: schema.string([rules.maxLength(1), rules.alpha()]),
+  })
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
-  public messages: CustomMessages = {}
+  public messages: CustomMessages = {
+    unique: '{{field}} tem que ser único',
+    exists: '{{field}} tem que existir',
+  }
 }
